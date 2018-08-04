97304b7641239c481ad50ba69351a4229cf3e226
kvora125 <41047220+kvora125@users.noreply.github.com>
2018-08-04 15:06:14 +0530

    Update lock.py

--- lock.py
+++ lock.py
@@ -1,1 +1,14 @@
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+import pymysql
+import time
+import serial
 
+def lock (int select):
+   while(GPIO.input(6)==GPIO.HIGH)
+   for(int i=0;i<4;i++)
+    if(select%10==1)
+      print("lock "+ i+1 + "opened")
+      
+   
